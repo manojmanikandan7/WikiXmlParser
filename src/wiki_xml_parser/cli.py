d608a051a868c0114c25da79c9bbd733436d81ae
Manoj Manikandan <manoj.manikandan@student.manchester.ac.uk>
2025-07-23 14:36:36 +0100

    fix indent error and add command help

--- src/wiki_xml_parser/cli.py
+++ src/wiki_xml_parser/cli.py
@@ -9,10 +9,10 @@ def run(
     output_dir: Annotated[str, typer.Argument(help="The output file directory for the transformed XML files")],
     corpus_name: Annotated[str, typer.Argument(help="The name of this corpus")],
 ):
-     x_parser = XmlParser()
-     x_parser.parse_corpus(Path(input_file_name), Path(output_dir), corpus_name)
+    x_parser = XmlParser()
+    x_parser.parse_corpus(Path(input_file_name), Path(output_dir), corpus_name)
 
 app = typer.Typer()
-app.command()(run)
+app.command(help="The command-line tool for parsing a Wikipedia-exported XML file to simplified XML files")(run)
 if __name__ == "__main__":
     app()
