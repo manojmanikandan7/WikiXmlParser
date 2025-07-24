97280eee8b7e2a9e71e8ebd8f6f4de33527a4c61
Manoj Manikandan <129196516+manojmanikandan7@users.noreply.github.com>
2025-07-24 10:43:12 +0100

    Delete src/wikipedia_xml_parser/run.py

--- src/wikipedia_xml_parser/run.py
+++
@@ -1,5 +0,0 @@
-from parser import XmlParser
-from pathlib import Path
-
-x_parser = XmlParser()
-x_parser.parse_corpus(Path("../../corpus/Covid-19.xml"), Path("../../outputs/COVID-19"), "COVID-19")
